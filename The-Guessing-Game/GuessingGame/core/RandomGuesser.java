a10a96a99921ddb7192414e3ed46f383eab60674
Harry Cortes <32592532+CortesHarry@users.noreply.github.com>
2017-10-14 19:18:53 -0400

    Update RandomGuesser.java
    Fixed minor indenting issues.

--- The-Guessing-Game/GuessingGame/core/RandomGuesser.java
+++ The-Guessing-Game/GuessingGame/core/RandomGuesser.java
@@ -11,9 +11,9 @@ public class RandomGuesser implements ClientController {
 		Member variables 
 	*/
 	/* Random object that we need to generate a random guess. */
-    private Random rand = new Random();
+    	private Random rand = new Random();
 	/* Configuration file that we need to access to see what the highest guessing bound is. */
-    private Configuration config;
+    	private Configuration config;
 
 	/* 
 		Member methods 
@@ -21,9 +21,9 @@ public class RandomGuesser implements ClientController {
 	/* Default constructor, error is thrown is Configuration file is not included.  */
 	public RandomGuesser() throws Exception { throw new Exception("Error! Configuration file not included!");  }
 	/* Main constructor, error is thrown is Configuration file is not included.  */
-    public RandomGuesser(Configuration config) { this.config = config; }
+	public RandomGuesser(Configuration config) { this.config = config; }
 	/* Randomized nextGuess() method; uses Random class to guess a random value.  */
-    public int nextGuess() { return this.rand.nextInt(config.getMaxNumber()) + 1; }
+    	public int nextGuess() { return this.rand.nextInt(config.getMaxNumber()) + 1; }
 	/* Randomized playAgain() method; uses Random class to guess whether or not the computer wants to play again.  */
 	public boolean playAgain() { return this.rand.nextInt(1) == 0; }
 }
