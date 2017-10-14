c14deb08b714fe8349d4f46555fc2704cc0d00f3
Harry Cortes <32592532+CortesHarry@users.noreply.github.com>
2017-10-14 19:09:50 -0400

    Updated Configuration.java
    Updated small indenting issues. Updated small syntax issues (e.g. replaced '//' with '/* */').

--- The-Guessing-Game/GuessingGame/core/Configuration.java
+++ The-Guessing-Game/GuessingGame/core/Configuration.java
@@ -9,9 +9,9 @@ public final class Configuration{
 	/* 
 		Member variables 
 	*/
-	//Holds the largest number that can be guessed. 
+	/* Holds the largest number that can be guessed.  */
 	private int maxNumber;
-	//Holds the number of guess attempts that can be made. 
+	/* Holds the number of guess attempts that can be made. */
 	private int allowedGuesses; 
 
 	/* 
@@ -23,16 +23,16 @@ public final class Configuration{
 	/* Main Constructor, uses arguments to initialize the maxNumber and the allowedGuesses */
 	public Configuration (int maxNumber, int allowedGuesses)  throws Exception {
 		if (maxNumber <= 0) { throw new Exception("maxNumber must be above 0."); }
-        if (allowedGuesses <= 0) { throw new Exception("allowedGuesses must be above 0."); }
-        this.maxNumber = maxNumber;
-        this.allowedGuesses = allowedGuesses;
+		if (allowedGuesses <= 0) { throw new Exception("allowedGuesses must be above 0."); }
+		this.maxNumber = maxNumber;
+		this.allowedGuesses = allowedGuesses;
 	}
 
 	/* 
 		Get-er methods 
 	*/
 	/* getMaxNumber() method, returns the maxNumber (highest bound that can be guessed) */
-    public int getMaxNumber() { return this.maxNumber; }
+    	public int getMaxNumber() { return this.maxNumber; }
 	/* getAllowedGuesses() method, returns the allowedGuesses (total attempts that the user gets) */
-    public int getAllowedGuesses() { return this.allowedGuesses; }
+    	public int getAllowedGuesses() { return this.allowedGuesses; }
 }
